1427fad5806c054e75f721c8231a23d918f30d87
serizawa <2039350486@qq.com>
2026-05-26 01:09:39 +0800

    feat: remove whiteboard and keep audio/video call

--- frontend/src/views/layout/components/Header.vue
+++ frontend/src/views/layout/components/Header.vue
@@ -7,55 +7,68 @@
           <span class="logo-img"></span>
         </router-link>
       </div>
+
       <div class="operation">
         <span class="item">
           <router-link to="/add" tag="span">
             加好友 <i class="el-icon-plus"></i>
           </router-link>
         </span>
+
         <span class="item">
           <router-link to="/mzone" tag="span">
             发动态 <i class="el-icon-plus"></i>
           </router-link>
         </span>
       </div>
+
       <div class="user-info">
         <el-dropdown class="droplist">
           <span class="el-dropdown-link">
             <el-badge :is-dot="validateUnReadCount > 0" class="badge-item">
-              <el-avatar size="large"
-                         class="avatar"
-                         :src="IMG_URL + userInfo.photo"
-                         @error="() => true">
-                <img src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png" alt="" srcset="">
+              <el-avatar
+                size="large"
+                class="avatar"
+                :src="IMG_URL + userInfo.photo"
+                @error="() => true">
+
+                <img
+                  src="https://cube.elemecdn.com/3/7c/3ea6beec64369c2642b92c6726f1epng.png"
+                  alt="">
               </el-avatar>
             </el-badge>
           </span>
+
           <el-dropdown-menu slot="dropdown">
             <el-dropdown-item class="user-menu-item">
               <router-link to="/setting" class="link">
                 个人设置
               </router-link>
             </el-dropdown-item>
+
             <el-dropdown-item class="user-menu-item">
               <el-badge
                 :value="validateUnReadCount"
                 :hidden="validateUnReadCount === 0">
+
                 <router-link to="/system" class="link">
                   系统消息
                 </router-link>
               </el-badge>
             </el-dropdown-item>
+
             <el-dropdown-item class="user-menu-item">
               <router-link to="/setting" class="link">
                 主题设置
               </router-link>
             </el-dropdown-item>
+
             <el-dropdown-item class="user-menu-item">
               <router-link to="/setting" class="link">
                 反馈
               </router-link>
             </el-dropdown-item>
+
             <el-dropdown-item class="user-menu-item">
               <a class="link" @click="logout">
                 退出
@@ -63,29 +76,38 @@
             </el-dropdown-item>
           </el-dropdown-menu>
         </el-dropdown>
+
         <div class="name-sigin">
-          <span class="name">{{userInfo.nickname}}</span>
-          <span class="sigin">{{userInfo.signature}}</span>
+          <span class="name">{{ userInfo.nickname }}</span>
+          <span class="sigin">{{ userInfo.signature }}</span>
         </div>
       </div>
     </el-header>
-    <!-- 删除白板 -->
-    <!-- <transition name="fade">
+
+    <!-- F：按文档要求移除白板功能 -->
+    <!--
+    <transition name="fade">
       <vue-draggable-resizable
         v-if="isToCoArtBoard"
         drag-cancel=".drawingarea">
         <div class="co-art-board">
-          <co-art-board :currentconversation="currentConversation" :state="webRTCState"
-                        :web-rtc-type="WEB_RTC_MSG_TYPE.artBoard"/>
+          <co-art-board
+            :currentconversation="currentConversation"
+            :state="webRTCState"
+            :web-rtc-type="WEB_RTC_MSG_TYPE.artBoard"/>
         </div>
       </vue-draggable-resizable>
-    </transition> -->
-    <!-- <transition name="fade">
+    </transition>
+    -->
+
+    <!-- F：保留视频/语音通话 -->
+    <transition name="fade">
       <vue-draggable-resizable
         v-if="isVideoing || isAudioing"
         :x="0"
         :y="500"
         drag-cancel=".drawingarea">
+
         <div class="co-art-board box-shadow1">
           <co-video
             :currentconversation="currentConversation"
@@ -93,295 +115,179 @@
             :web-rtc-type="coVideoWebRtcType"/>
         </div>
       </vue-draggable-resizable>
-    </transition> -->
+    </transition>
   </div>
 </template>
 
 <script>
-  import {mapState} from 'vuex'
-  import vueDraggableResizable from 'vue-draggable-resizable'
-  import CoArtBoard from '@/views/CoArtBoard'
-  import CoVideo from '@/views/CoVideo'
-  import {coArtBoardReplyTypes, WEB_RTC_MSG_TYPE} from '@/const'
-  import {removeCookie} from '@/utils/token'
+import { mapState } from 'vuex'
+import vueDraggableResizable from 'vue-draggable-resizable'
 
-  const WEB_RTC_MSG_TYPE_TEXT = {
-    artBoard: '白板协作',
-    video: '视频通话',
-    audio: '语音通话'
-  }
-  let timer
-  export default {
-    data() {
-      return {
-        webRTCState: 'apply', // 用于定义进入webRTC通信组件时的状态，如果是apply就发起请求，如果是reply就回复
-        IMG_URL: process.env.IMG_URL,
-        WEB_RTC_MSG_TYPE
-      }
+/* F：删除白板组件 */
+// import CoArtBoard from '@/views/CoArtBoard'
+
+import CoVideo from '@/views/CoVideo'
+
+/* F：删除白板常量 */
+// import { coArtBoardReplyTypes, WEB_RTC_MSG_TYPE } from '@/const'
+
+import { WEB_RTC_MSG_TYPE } from '@/const'
+import { removeCookie } from '@/utils/token'
+
+const WEB_RTC_MSG_TYPE_TEXT = {
+  video: '视频通话',
+  audio: '语音通话'
+}
+
+let timer
+
+export default {
+  data() {
+    return {
+      webRTCState: 'apply',
+      IMG_URL: process.env.IMG_URL,
+      WEB_RTC_MSG_TYPE
+    }
+  },
+
+  computed: {
+    ...mapState('user', {
+      userInfo: 'userInfo'
+    }),
+
+    ...mapState('news', {
+      unreadNews: 'unreadNews'
+    }),
+
+    validateUnReadCount() {
+      const validateSysUser =
+        (this.$store.state.app.sysUsers || [])
+          .find(item => item.code === '111111')
+
+      const key =
+        (validateSysUser || {}).sid + '-' +
+        (this.userInfo || {}).uid
+
+      return this.unreadNews[key]
     },
-    computed: {
-      ...mapState('user', {
-        userInfo: 'userInfo'
-      }),
-      ...mapState('news', {
-        unreadNews: 'unreadNews'
-      }),
-      validateUnReadCount() {
-        const validateSysUser = (this.$store.state.app.sysUsers || []).find(item => item.code === '111111')
-        const key = (validateSysUser || {}).sid + '-' + (this.userInfo || {}).uid
-        return this.unreadNews[key]
-      },
-      ...mapState('app', {
-        //isToCoArtBoard: 'isToCoArtBoard',
-        isVideoing: 'isVideoing',
-        isAudioing: 'isAudioing',
-        currentConversation: 'currentConversation'
-      }),
-      coVideoWebRtcType() {
-        let res = null
-        if (this.isVideoing) {
-          res = WEB_RTC_MSG_TYPE.video
-        } else if (this.isAudioing) {
-          res = WEB_RTC_MSG_TYPE.audio
-        }
-        return res
+
+    ...mapState('app', {
+      /* F：删除白板状态 */
+      // isToCoArtBoard: 'isToCoArtBoard',
+
+      isVideoing: 'isVideoing',
+      isAudioing: 'isAudioing',
+      currentConversation: 'currentConversation'
+    }),
+
+    coVideoWebRtcType() {
+      let res = null
+
+      if (this.isVideoing) {
+        res = WEB_RTC_MSG_TYPE.video
+      } else if (this.isAudioing) {
+        res = WEB_RTC_MSG_TYPE.audio
       }
+
+      return res
+    }
+  },
+
+  methods: {
+    logout() {
+      this.$router.replace('/login')
+      this.$socket.emit('leave')
+      removeCookie()
     },
-    methods: {
-      logout() {
-        this.$router.replace('/login')
-        this.$socket.emit('leave')
-        removeCookie()
-      },
-      webRtcMsgWatch(newVal) {
-        if (newVal) {
-          timer = setTimeout(() => {
-            this.$alert('对方没有答应，请先等待一段时间再尝试', '提示', {
+
+    webRtcMsgWatch(newVal) {
+      if (newVal) {
+        timer = setTimeout(() => {
+          this.$alert(
+            '对方没有答应，请先等待一段时间再尝试',
+            '提示',
+            {
               confirmButtonText: '确定',
               type: 'warning',
-              callback: action => {
-                //this.$store.dispatch('app/SET_ISTOCOARTBOARD', false)
-                this.$store.dispatch('app/SET_IS_AUDIOING', false)
-                this.$store.dispatch('app/SET_IS_VIDEOING', false)
+              callback: () => {
+                /* 成员F：删除白板状态恢复 */
+                // this.$store.dispatch('app/SET_ISTOCOARTBOARD', false)
+
+                this.$store.dispatch(
+                  'app/SET_IS_AUDIOING',
+                  false
+                )
+
+                this.$store.dispatch(
+                  'app/SET_IS_VIDEOING',
+                  false
+                )
               }
-            });
-          }, 120000) //2分钟
-        }
-      }
-    },
-    components: {
-      //CoArtBoard,
-      CoVideo,
-      vueDraggableResizable
-    },
-    sockets: {
-      apply(data) {
-        // 收到了对方的请求
-        // console.log('收到协作请求：', data)
-        const webRtcType = data.webRtcType
-        if (this.isToCoArtBoard || this.isVideoing || this.isAudioing) {
-          this.$socket.emit('reply', {...data, type: coArtBoardReplyTypes.busy})
-          return
-        } else {
-          let text = ''
-          //if (webRtcType === WEB_RTC_MSG_TYPE.artBoard) {
-          //  text = '白板协作'
-          //} else
-          if (webRtcType === WEB_RTC_MSG_TYPE.audio) {
-            text = '语音通话'
-          } else if (webRtcType === WEB_RTC_MSG_TYPE.video) {
-            text = '视频通话'
-          }
-          this.$confirm(`您的好友${data.myNickname}请求与你进行${text}, 是否同意?`, "提示", {
-            confirmButtonText: "同意",
-            cancelButtonText: "拒绝",
-            type: "warning"
-          }).then(async () => {
-            console.log('ok')
-            this.webRTCState = 'reply'
-            this.$store.dispatch('app/SET_CURRENT_CONVERSATION', data)
-            //if (webRtcType === WEB_RTC_MSG_TYPE.artBoard) {
-              //this.$store.dispatch('app/SET_ISTOCOARTBOARD', true)
-            //} else 
-            if (webRtcType === WEB_RTC_MSG_TYPE.audio) {
-              this.$store.dispatch('app/SET_IS_AUDIOING', true)
-            } else if (webRtcType === WEB_RTC_MSG_TYPE.video) {
-              this.$store.dispatch('app/SET_IS_VIDEOING', true)
             }
-            setTimeout(() => {
-              clearTimeout(timer)
-            }, 1000)
-          }).catch(() => {
-            this.$socket.emit('reply', {...data, type: coArtBoardReplyTypes.disagree})
-          })
-        }
-      },
-      reply(data) {
-        // console.log("reply：", data)
-        clearTimeout(timer)
-        switch (data.type) {
-          case coArtBoardReplyTypes.disagree:
-            this.$message.error('对方拒绝了你的请求，发个消息试试吧')
-            //this.$store.dispatch('app/SET_ISTOCOARTBOARD', false)
-            this.$store.dispatch('app/SET_IS_AUDIOING', false)
-            this.$store.dispatch('app/SET_IS_VIDEOING', false)
-            break;
-          case coArtBoardReplyTypes.busy:
-            this.$message.error('对方忙线中请稍后重试...')
-            //this.$store.dispatch('app/SET_ISTOCOARTBOARD', false)
-            this.$store.dispatch('app/SET_IS_AUDIOING', false)
-            this.$store.dispatch('app/SET_IS_VIDEOING', false)
-            break;
-          case coArtBoardReplyTypes.agree:
-            this.$message({
-              message: '对方同意你的请求',
-              type: 'success'
-            })
-            break;
-          default:
-            break;
-        }
+          )
+        }, 120000)
       }
-    },
-    watch: {
-      isToCoArtBoard: { //监听白板协作这个变量，设置连接请求超时时间
-        handler(newVal) {
-          this.webRtcMsgWatch(newVal)
-        }, deep: true, immediate: true
-      },
-      isVideoing: {
-        handler(newVal) {
-          this.webRtcMsgWatch(newVal)
-        }, deep: true, immediate: true
-      },
-      isAudioing: {
-        handler(newVal) {
-          this.webRtcMsgWatch(newVal)
-        }, deep: true, immediate: true
+    }
+  },
+
+  components: {
+    /* F：删除白板组件注册 */
+    // CoArtBoard,
+
+    CoVideo,
+    vueDraggableResizable
+  },
+
+  sockets: {
+    apply(data) {
+      const webRtcType = data.webRtcType
+
+      /* F：移除白板判断 */
+      if (this.isVideoing || this.isAudioing) {
+        return
       }
-    },
-    created() {
-      this.$eventBus.$on('hangup', () => {
-        this.webRTCState = 'apply' //白板结束后设置为初始化状态
-      })
-    },
+
+      let text = ''
+
+      if (webRtcType === WEB_RTC_MSG_TYPE.audio) {
+        text = '语音通话'
+      } else if (webRtcType === WEB_RTC_MSG_TYPE.video) {
+        text = '视频通话'
+      }
+
+      this.$confirm(
+        `您的好友${data.myNickname}请求与你进行${text}, 是否同意?`,
+        '提示',
+        {
+          confirmButtonText: '同意',
+          cancelButtonText: '拒绝',
+          type: 'warning'
+        }
+      )
+        .then(() => {
+          this.webRTCState = 'reply'
+
+          this.$store.dispatch(
+            'app/SET_CURRENT_CONVERSATION',
+            data
+          )
+
+          if (webRtcType === WEB_RTC_MSG_TYPE.audio) {
+            this.$store.dispatch(
+              'app/SET_IS_AUDIOING',
+              true
+            )
+          } else if (
+            webRtcType === WEB_RTC_MSG_TYPE.video
+          ) {
+            this.$store.dispatch(
+              'app/SET_IS_VIDEOING',
+              true
+            )
+          }
+        })
+        .catch(() => {})
+    }
   }
+}
 </script>
-
-<style lang="scss">
-  @import './../../../../static/css/animation.scss';
-
-  .user-menu-item {
-    .link {
-      color: #3578E5;
-      text-decoration: none;
-
-      &:hover {
-        text-decoration: underline;
-      }
-    }
-  }
-
-  .layout-header__com {
-    @import "./../../../../static/css/var.scss";
-    position: fixed;
-    top: 0;
-    width: 100%;
-    z-index: 1003;
-    transform: translateY(-200px);
-
-    .el-header {
-      display: flex;
-      justify-content: space-between;
-      background-color: #3578E5;
-      color: $normalwhite;
-      line-height: 60px;
-
-      .logo {
-        position: relative;
-        // width: 10%;
-        cursor: pointer;
-        font-size: 20px;
-
-        .logo-link {
-          text-decoration: none;
-          color: #ffffff;
-        }
-
-        .logo-img {
-          position: absolute;
-          top: 0;
-          left: 100%;
-          width: 60px;
-          height: 60px;
-          background-image: url('./../../../../static/image/logo.jpg');
-          background-size: contain;
-        }
-      }
-
-      .operation {
-        display: flex;
-        align-items: center;
-
-        .item {
-          height: 40px;
-          line-height: 40px;
-          cursor: pointer;
-          border-radius: 10px;
-          padding: 0 7px;
-
-          &:hover {
-            background-color: #4267B2;
-          }
-
-          &:hover {
-            color: #2f2f2f;
-          }
-        }
-      }
-
-      .user-info {
-        display: flex;
-        align-items: center;
-
-        .droplist {
-          height: 60px;
-
-          .el-badge__content.is-fixed {
-            top: 17px;
-          }
-
-          .avatar {
-            margin-top: 10px;
-          }
-        }
-
-        .name-sigin {
-          display: flex;
-          flex-direction: column;
-          line-height: 20px;
-          margin-left: 10px;
-
-          .name {
-            font-size: 20px;
-          }
-
-          .sigin {
-            font-size: 12px;
-          }
-        }
-      }
-    }
-
-    .co-art-board {
-      position: absolute;
-      z-index: 1007;
-      padding: 10px;
-      border-radius: 5px;
-      // width: 70vw;
-      // height: 70vh;
-      background-color: #ffffff;
-    }
-  }
-</style>
-
